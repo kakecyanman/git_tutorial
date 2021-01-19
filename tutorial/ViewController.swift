d50faffd56139cf905cddc19e02a117f93a80b81
kakecyanman <a.k.2903soccer@icloud.com>
2021-01-20 00:15:41 +0900

    first commit

---
+++ tutorial/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  tutorial
+//
+//  Created by 阿迦井翔 on 2021/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("チュートリアル")
+    }
+
+    @IBAction func Button(_ sender: Any) {
+        print("ボタンが押されたよ")
+    }
+    
+}
+
